9347bc7c38f93aa220f9eb14a322fe24b40efb90
vedsudhar <122882485+vedsudhar@users.noreply.github.com>
2023-05-28 18:32:37 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,18 @@
+
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+    
+}
+
+
+
